cf983261d248a225edb97a01d9b33683826e6008
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2025-04-16 10:21:28 +0200

    adding dates to transactions

--- script.js
+++ script.js
@@ -2,10 +2,23 @@
 
 // Data
 const account1 = {
-  owner: "John Stevenson",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  owner: "Jonas Schmedtmann",
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    "2025-11-18T21:31:17.178Z",
+    "2025-12-23T07:42:02.383Z",
+    "2025-01-28T09:15:04.904Z",
+    "2025-04-01T10:17:24.185Z",
+    "2025-05-08T14:11:59.604Z",
+    "2025-07-26T17:01:17.194Z",
+    "2025-07-28T23:36:17.929Z",
+    "2025-08-01T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
@@ -13,23 +26,22 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    "2025-11-01T13:15:33.035Z",
+    "2025-11-30T09:48:16.867Z",
+    "2025-12-25T06:04:23.907Z",
+    "2025-01-25T14:18:46.235Z",
+    "2025-02-05T16:33:06.386Z",
+    "2025-04-10T14:43:26.374Z",
+    "2025-06-25T18:49:59.371Z",
+    "2025-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
-const account3 = {
-  owner: "Steven Thomas Williams",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: "Frank Delano",
-  movements: [390, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector(".welcome");
@@ -67,10 +79,18 @@ const displayMovements = function (acc, sort = false) {
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
+    const date = new Date(acc.movementsDates[i]);
+
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    const displayDate = `${day}/${month}/${year}`;
+
     const html = `
      <div class="movements__row">
         <div class="movements__type movements__type--${type}"> 
         ${i + 1} ${type}</div>
+        <div class=movements_date>${displayDate}</div>
         <div class="movements__value">${mov.toFixed(2)}</div>
       </div>
     `;
